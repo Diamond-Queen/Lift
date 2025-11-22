da1d1c86a733cae324db42753381e46f8164f1ef
Diamond-Queen <q.neverhadthis@gmail.com>
2025-11-23 03:58:58 +0930

    changes

--- pages/index.jsx
+++ pages/index.jsx
@@ -68,9 +68,6 @@ export default function Home() {
                 <Link href="/career" className={`${styles.btn} ${styles.accent}`}>
                   Lift Career
                 </Link>
-                <Link href="/login" className={`${styles.btn} ${styles.ghost}`}>
-                  Login
-                </Link>
             </nav>
           </div>
 
